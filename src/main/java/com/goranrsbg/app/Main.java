3dbafef39e304828dd1468f44fa2c5483aae81bc
goranrsbg <goranrsbg@themachine>
2019-01-07 23:05:25 +0100

    find server

--- src/main/java/com/goranrsbg/app/Main.java
+++ src/main/java/com/goranrsbg/app/Main.java
@@ -1,5 +1,9 @@
 package com.goranrsbg.app;
 	
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Properties;
 
 import javafx.application.Application;
@@ -15,6 +19,7 @@ public class Main extends Application {
 	public void start(final Stage stage) {
 		try {
 			Properties props = new Properties();
+			findServer();
 			props.load(getClass().getResourceAsStream("/app.properties"));
 			Parent root = (Parent)FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
 			stage.setScene(new Scene(root));
@@ -31,6 +36,31 @@ public class Main extends Application {
 		System.out.println("Poštonoša ugašen.");
 	}
 	
+	private String findServer() throws SocketException {
+		Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+		while (networkInterfaces.hasMoreElements()) {
+			NetworkInterface networkInterface = (NetworkInterface) networkInterfaces.nextElement();
+			displayInformation(networkInterface);
+		}
+		return "none";
+	}
+	
+	private void displayInformation(NetworkInterface networkInterface) {
+		log("Display name: " + networkInterface.getDisplayName());
+		log("Name: " + networkInterface.getName());
+		Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+		while (inetAddresses.hasMoreElements()) {
+			InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
+			log("InetAddress: " + inetAddress);
+		}
+	}
+	
+	
+
+	private void log(String text) {
+		System.out.println(text);
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
