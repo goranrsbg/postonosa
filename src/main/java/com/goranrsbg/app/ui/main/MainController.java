e3cb629a0eff80d98b3d1d9a737e632defb95529
goranrsbg <goranrsbg@themachine>
2019-01-16 00:41:28 +0100

    map add is working

--- src/main/java/com/goranrsbg/app/ui/main/MainController.java
+++ src/main/java/com/goranrsbg/app/ui/main/MainController.java
@@ -1,24 +1,49 @@
 package com.goranrsbg.app.ui.main;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
 
 public class MainController implements Initializable {
 
-    @FXML
-    private Label informationLabel;
+	@FXML
+	private StackPane stackPane;
 
-    @FXML
-    private StackPane stackPane;
+	@FXML
+	private ImageView imageView;
+
+	private Window window;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		Platform.runLater(() -> {
+			window = stackPane.getScene().getWindow();
+		});
+	}
+
+	@FXML
+	void openMap(ActionEvent event) {
+		System.out.println("Time to open map.");
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Izaberi Mapu");
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Images", "*_map.jpg"));
+		File file = fileChooser.showOpenDialog(window);
+		if(file != null) {
+			Image theMap = new Image(file.toURI().toString());
+			imageView.setImage(theMap);
+			imageView.fitWidthProperty().bind(theMap.widthProperty());
+		}
 	}
 
 }
