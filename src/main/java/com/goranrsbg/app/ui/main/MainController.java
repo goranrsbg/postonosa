aa8052ee4c38f1046d998e56cbdfc245b1cc2efd
goranrsbg <goranrsbg@themachine>
2019-01-17 22:21:12 +0100

    for zooming viewPort is way to go, to be continued...

--- src/main/java/com/goranrsbg/app/ui/main/MainController.java
+++ src/main/java/com/goranrsbg/app/ui/main/MainController.java
@@ -8,7 +8,9 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
@@ -26,6 +28,18 @@ public class MainController implements Initializable {
 
 	@FXML
 	private Slider zoomSlider;
+
+	@FXML
+	private TextField x;
+
+	@FXML
+	private TextField y;
+
+	@FXML
+	private TextField w;
+
+	@FXML
+	private TextField h;
 
 	private Window window;
 	private FileChooser fileChooser;
@@ -46,21 +60,34 @@ public class MainController implements Initializable {
 		if (file != null) {
 			setTheMap(file.toURI().toString());
 		}
+		event.consume();
 	}
-	
+
+	@FXML
+	void readViewPort(ActionEvent event) {
+		Rectangle2D rec2d = getRec2d();
+		imageView.setViewport(rec2d);
+		event.consume();
+	}
+
+	private Rectangle2D getRec2d() {
+		if (x.getText() == "" || y.getText() == "" || h.getText() == "" || w.getText() == "") {
+			return null;
+		}
+		int X = Integer.parseInt(x.getText());
+		int Y = Integer.parseInt(y.getText());
+		int W = Integer.parseInt(w.getText());
+		int H = Integer.parseInt(h.getText());
+		return new Rectangle2D(X, Y, W, H);
+	}
+
 	private void setTheMap(String url) {
 		Image theMap = new Image(url);
 		imageView.setImage(theMap);
 		imageView.fitWidthProperty().bind(theMap.widthProperty());
+		imageView.fitHeightProperty().bind(theMap.heightProperty());
 		imageView.scaleXProperty().bind(zoomSlider.valueProperty());
 		imageView.scaleYProperty().bind(zoomSlider.valueProperty());
 	}
 
 }
-
-
-
-
-
-
-
